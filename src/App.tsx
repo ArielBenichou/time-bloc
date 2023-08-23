e1f2a5b77582f9449f6e140c3e42e6cc6de8c91e
ArielBenichou <videojr3000@users.noreply.github.com>
2023-08-23 13:19:51 +0300

    feat: better ui for calendar, simpler button for week pagination, with better month title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createSignal } from 'solid-js';
 import type { Component } from 'solid-js';
 import Calendar from './components/Calendar';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import weekday from 'dayjs/plugin/weekday';
 import SlotsManager from './components/SlotsManager';
@@ -24,10 +24,23 @@ function getCurrentSunday() {
     return sundayAtMidnight;
 }
 
+function formatWeek(date: Dayjs) {
+    const startOfWeek = date.startOf('week');
+    const endOfWeek = date.endOf('week');
+
+    const startMonth = startOfWeek.format('MMMM');
+    const endMonth = endOfWeek.format('MMMM');
+    const year = startOfWeek.format('YYYY');
+
+    if (startMonth === endMonth) {
+        return startOfWeek.format('MMMM YYYY');
+    } else {
+        return `${startMonth.slice(0, 3)}-${endMonth.slice(0, 3)} ${year}`;
+    }
+}
+
 const App: Component = () => {
     const [currentWeek, setCurrentWeek] = createSignal(getCurrentSunday());
-
-    // TODO: Define your task slots and logic here
 
     return (
         <div class="flex flex-col h-screen p-2">
@@ -38,21 +51,20 @@ const App: Component = () => {
             </div>
             <div class="flex h-5/6">
                 <div class="w-3/4 p-4">
-                    <div class="mt-auto flex justify-end gap-2 w-full mb-2">
-                        <div>{currentWeek().toISOString()}</div>
+                    <div class="mt-auto flex justify-start items-center gap-2 w-full mb-2">
                         {/* Chevron arrows to scroll through the weeks */}
                         <button
-                            class="px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded"
+                            class="px-4 py-2 text-black bg-gray-100 hover:bg-gray-200 rounded-full"
                             onClick={() => setCurrentWeek(currentWeek().subtract(7, 'days'))}
                         >
-                            &lt; Previous Week
-                        </button>
+                            &lt;                        </button>
                         <button
-                            class="px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded"
+                            class="px-4 py-2 text-black bg-gray-100 hover:bg-gray-200 rounded-full"
                             onClick={() => setCurrentWeek(currentWeek().add(7, 'days'))}
                         >
-                            Next Week &gt;
+                            &gt;
                         </button>
+                        <div class="text-xl font-bold">{formatWeek(currentWeek())}</div>
                     </div>
                     <Calendar currentWeek={currentWeek()} />
                 </div>
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -20,69 +20,42 @@ const Calendar: Component<{ currentWeek: Dayjs }> = (props) => {
         // Add more calendar events as needed
     ]);
 
-    const [draggingEvent, setDraggingEvent] = createSignal<CalendarEvent | null>(null);
-    const [isDragging, setIsDragging] = createSignal(false);
-
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    const handleDragStart = (event: CalendarEvent) => {
-        setDraggingEvent(event);
-        setIsDragging(true);
-    };
-
-    const handleDrag = (event: MouseEvent) => {
-        if (isDragging() && draggingEvent()) {
-            // Update event start and end times based on cursor position
-            // Implement snapping behavior to time slots
-        }
-    };
-
-    const handleDragEnd = () => {
-        if (draggingEvent()) {
-            // Update the event data in the calendarEvents signal
-            // Reset dragging state
-        }
-    };
-
     return (
-        <div class="border border-gray-300 rounded p-4 h-full overflow-scroll" onMouseMove={handleDrag} onMouseUp={handleDragEnd}>
-            <h2 class="text-xl font-semibold mb-4">Calendar</h2>
-            <div class="grid grid-cols-7 gap-4">
-                {daysOfWeek.map((day, index) => {
-                    const dayDate = props.currentWeek.add(index, 'day');
-                    return (
-                        <div class="border border-gray-300 p-2" >
-                            <div class="text-lg font-semibold mb-2">{day}</div>
-                            <div class="text-gray-500 mb-2">{dayDate.format('ddd, MMM D')}</div>
-                            {/* Render hour slots */}
-                            {Array.from({ length: 24 }).map((_, hour) => (
-                                <div class="h-10 border-t border-gray-300" >
-                                    {calendarEvents().map(event => {
-                                        const eventStartTime = dayjs(event.startTime);
-                                        const eventEndTime = dayjs(event.endTime);
-                                        if (
-                                            eventStartTime.isSame(dayDate, 'day') &&
-                                            eventStartTime.hour() <= hour &&
-                                            eventEndTime.hour() > hour
-                                        ) {
-                                            return (
-                                                <div
-                                                    class="bg-blue-200 h-10 p-1 rounded text-sm cursor-pointer"
-
-                                                    onMouseDown={() => handleDragStart(event)}
-                                                >
-                                                    {event.title}
-                                                </div>
-                                            );
-                                        }
-                                        return null;
-                                    })}
-                                </div>
-                            ))}
-                        </div>
-                    );
-                })}
-            </div>
+        <div class="border border-gray-300 grid grid-cols-7 h-full overflow-scroll rounded">
+            {daysOfWeek.map((day, index) => {
+                const dayDate = props.currentWeek.add(index, 'day');
+                return (
+                    <div class="border border-gray-300 p-2" >
+                        <div class="text-lg font-semibold mb-2">{day}</div>
+                        <div class="text-gray-500 mb-2">{dayDate.format('ddd, MMM D')}</div>
+                        {/* Render hour slots */}
+                        {Array.from({ length: 24 }).map((_, hour) => (
+                            <div class="h-10 border-t border-gray-300" >
+                                {calendarEvents().map(event => {
+                                    const eventStartTime = dayjs(event.startTime);
+                                    const eventEndTime = dayjs(event.endTime);
+                                    if (
+                                        eventStartTime.isSame(dayDate, 'day') &&
+                                        eventStartTime.hour() <= hour &&
+                                        eventEndTime.hour() > hour
+                                    ) {
+                                        return (
+                                            <div
+                                                class="bg-blue-200 h-10 p-1 rounded text-sm cursor-pointer"
+                                            >
+                                                {event.title}
+                                            </div>
+                                        );
+                                    }
+                                    return null;
+                                })}
+                            </div>
+                        ))}
+                    </div>
+                );
+            })}
         </div>
     );
 };
