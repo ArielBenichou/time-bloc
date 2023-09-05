cdfba9829a0aac9f000ab16194e7b3ff8329f94a
ArielBenichou <videojr3000@users.noreply.github.com>
2023-09-05 20:44:53 +0300

    feat: add user avatar + remove rn-felx and use base classes

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,15 @@ import { Dayjs } from 'dayjs';
 const App: Component = () => {
     const [currentWeek, setCurrentWeek] = createSignal(getCurrentSunday());
     return (
-        <div class="rn-flex flex-1 p-2 h-full bg-slate-100">
+        <div class="flex flex-col min-h-0 flex-1 p-2 h-full bg-slate-100">
             <div class="flex justify-between">
                 <h1 class="text-4xl">Time<b>Bloc</b></h1>
-                { /* <UserProfileBadge /> */}
-                <p>User</p>
+                <UserAvatar />
             </div>
 
-            <div class="rn-flex flex-col gap-4 m-2 flex-1">
+            <div class="flex flex-col min-h-0 gap-4 m-2 flex-1">
                 <ToolsHeader currentWeek={currentWeek()} setCurrentWeek={setCurrentWeek} />
-                <div class="rn-flex flex-row gap-4 flex-1">
+                <div class="flex flex-row min-h-0 gap-4 flex-1">
                     <div class="flex flex-1 flex-col basis-3/4 gap-4">
                         <Calendar currentWeek={currentWeek()} />
                     </div>
@@ -31,6 +30,36 @@ const App: Component = () => {
         </div>
     );
 };
+
+const UserAvatar: Component = () => {
+    // TODO: make texts dynmaic, make the button do somthing
+    const [isOpen, setIsOpen] = createSignal(false);
+    return (
+        <>
+            <div class="relative" onClick={() => setIsOpen(!isOpen())}>
+                <div class="relative inline-flex h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
+                    <span class="font-medium text-gray-600 dark:text-gray-300">AB</span>
+                </div>
+                <span class="absolute left-7 top-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-green-400 dark:border-gray-800"></span>
+
+                <div id="userDropdown" class={(isOpen() ? "" : "hidden ") + "right-0 z-10 absolute mt-1 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700"}>
+                    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                        <div>Ariel Benichou</div>
+                        <div class="truncate font-medium">arielbenichou@nomail.net</div>
+                    </div>
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                        </li>
+                    </ul>
+                    <div class="py-1">
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
 
 const ToolsHeader: Component<{ currentWeek: Dayjs, setCurrentWeek: Setter<Dayjs> }> = (props) => {
     return (
